560376d4c79afc3b9b3cbb78ff1339c6272392d1
Kareem James <kareemdjames@gmail.com>
2017-10-24 22:38:57 -0400

    added controller.js, renamed model.js, added route to server

---
+++ controllers/controller.js
@@ -0,0 +1,15 @@
+let Todo = require('../models/Todo');
+
+const todoController = {};
+
+todoController.index = (req,res) => {
+  Todo.findAll()
+    .then(todos => {
+      res.status(200).render('/todos/todos-index', {
+        todos: todos,
+      });
+    }).catch(err => {
+    console.log(err);
+    res.status(500).json({error: err});
+  });
+}
--- server.js
+++ server.js
@@ -10,9 +10,22 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
+//setting up ejs
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'public'));
+
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride('method'));
 
 //routes
-app.get('/', function (req, res) {
-  res.send('Hello World')
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.use('*', (req, res) => {
+  res.status(404).send('Not Found');
 });
 
