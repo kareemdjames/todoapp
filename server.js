80ea6e4d22b27ca5ba145cdb21527019ef5307ed
Kareem James <kareemdjames@gmail.com>
2017-10-26 20:52:39 -0400

    added initial auth functionality

--- server.js
+++ server.js
@@ -3,8 +3,12 @@ const logger = require('morgan');
 const path = require('path');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const passport = require('passport');
 //Init app
 const app = express();
+require('dotenv').config();
 // specify port
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
@@ -19,6 +23,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(methodOverride('_method'));
+app.use(cookieParser());
+app.use(session({
+  secret: process.env.SECRET_KEY,
+  resave: false,
+  saveUninitialized: true,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 //routes
 app.get('/', (req, res) => {
@@ -28,7 +40,11 @@ app.get('/', (req, res) => {
 const todosRoutes = require('./routes/routes');
 app.use('/todos', todosRoutes);
 
+const authRoutes = require('./routes/auth-routes');
+app.use('/auth', authRoutes);
+const userRoutes = require('./routes/user-routes');
+app.use('/user', userRoutes);
+
 app.use('*', (req, res) => {
   res.status(404).send('Not Found');
 });
-
